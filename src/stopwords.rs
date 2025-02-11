c28ad42fc6a8410adcab6ee8535950d91f55f1c1
Peter Robinson <peter.robinson@jax.org>
2025-02-11 09:31:58 +0100

    stop word check

---
+++ src/stopwords.rs
@@ -0,0 +1,42 @@
+use std::collections::HashSet;
+
+
+
+
+
+pub fn is_stop(token: &str) -> bool {
+    const STOP: &[&str] = &[
+        "a", "the", "and","of", "in", "to", "on", "an", "with"
+    ];
+    STOP.contains(token)
+}
+
+
+#[cfg(test)]
+mod test {
+    use std::assert_eq;
+
+    use super::*;
+
+    #[test]
+    fn test_stop() {
+        let tests = vec![
+             ("a", true),
+             ("the", true),
+             ("and", true),
+             ("of", true), 
+             ("in" true),
+             ("to", true)
+             ("on", true),
+             ( "an", true),
+             ("with" true),
+             ("tada", false),
+             ("red", 1)
+        ];
+        for test in tests {
+            let is_stop_w = is_stop(test.0);
+            assert_eq!(test.1, is_stop_w);
+        }
+    }
+
+}
