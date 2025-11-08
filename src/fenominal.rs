a93be65316d1538686d8255a97d12659ab01251e
Peter Robinson <peter.robinson@jax.org>
2025-11-08 10:26:09 +0100

    Updates

--- src/fenominal.rs
+++ src/fenominal.rs
@@ -1,66 +1,3 @@
-//! Implementation of the Fenominal text mining algorithm.
-//!
-//! ## Configure Fenominal
-//!
-//! [`Fenominal`] is created from [`ontolius::ontology::csr::FullCsrOntology`],
-//! which can, in turn, be loaded from a HPO JSON file:
-//!
-//! ```
-//! use std::fs::File;
-//! use std::io::BufReader;
-//! use std::sync::Arc;
-//! use flate2::bufread::GzDecoder;
-//! use fenominal::fenominal::Fenominal;
-//! use ontolius::io::OntologyLoaderBuilder;
-//! use ontolius::ontology::csr::FullCsrOntology;
-//!
-//! // Load HPO from the repo, use `flate2` to decompress on the fly
-//! let hp_path = "resources/hp.v2025-03-03.json.gz";
-//! let loader = OntologyLoaderBuilder::new().obographs_parser().build();
-//! let hpo: FullCsrOntology = loader.load_from_read(
-//!             GzDecoder::new(BufReader::new(File::open(hp_path).expect("HPO should be readable")))
-//!           ).expect("HPO should be well formatted");
-//! let hpo = Arc::new(hpo);
-//! // Configure Fenominal
-//! let fenominal = Fenominal::new(hpo);
-//! ```
-//!
-//! ## Use Fenominal
-//! 
-//! There are two implementations of [`TextMiner`] trait,
-//! one for mining [`FenominalHit`]s and the other for getting [`TermId`]s.
-//! 
-//!
-//! ### Example
-//! 
-//! Get [`FenominalHit`]s for an example text:
-//! 
-//! ```
-//! use std::fs::File;
-//! use std::io::BufReader;
-//! use std::sync::Arc;
-//! use flate2::bufread::GzDecoder;
-//! use fenominal::fenominal::Fenominal;
-//! use ontolius::io::OntologyLoaderBuilder;
-//! use ontolius::ontology::csr::FullCsrOntology;
-//! let hp_path = "resources/hp.v2025-03-03.json.gz";
-//! let loader = OntologyLoaderBuilder::new().obographs_parser().build();
-//! let hpo: FullCsrOntology = loader.load_from_read(
-//!              GzDecoder::new(BufReader::new(File::open(hp_path).expect("HPO should be readable")))
-//!            ).expect("HPO should be well formatted");
-//! let hpo = Arc::new(hpo);
-//! let fenominal = Fenominal::new(hpo);
-//! use fenominal::fenominal::FenominalHit;
-//!
-//! // Perform text mining
-//! let text = "Intellectual disability, macrocephaly, scoliosis";
-//! let hits: Vec<FenominalHit> = fenominal.process(&text);
-//!
-//! let labels: Vec<_> = hits.iter().map(|hit| &hit.label).collect();
-//! assert_eq!(labels, &["Intellectual disability", "Macrocephaly", "Scoliosis"]);
-//! ```
-//!
-
 use std::fmt;
 use std::ops::Range;
 use std::sync::Arc;
@@ -99,6 +36,7 @@ impl FenominalHit {
         }
     }
 
+    /// get the start/end position of a 'Hit'
     pub fn get_span(&self) -> Range<usize> {
         Clone::clone(&self.span)
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,67 @@
+//! Implementation of the Fenominal text mining algorithm.
+//!
+//! ## Configure Fenominal
+//!
+//! A [`fenominal::Fenominal`] struct is created from [`ontolius::ontology::csr::FullCsrOntology`],
+//! which can, in turn, be loaded from a HPO JSON file:
+//!
+//! ```
+//! use std::fs::File;
+//! use std::io::BufReader;
+//! use std::sync::Arc;
+//! use flate2::bufread::GzDecoder;
+//! use fenominal::fenominal::Fenominal;
+//! use ontolius::io::OntologyLoaderBuilder;
+//! use ontolius::ontology::csr::FullCsrOntology;
+//!
+//! // Load HPO from the repo, use `flate2` to decompress on the fly
+//! let hp_path = "resources/hp.v2025-03-03.json.gz";
+//! let loader = OntologyLoaderBuilder::new().obographs_parser().build();
+//! let hpo: FullCsrOntology = loader.load_from_read(
+//!             GzDecoder::new(BufReader::new(File::open(hp_path).expect("HPO should be readable")))
+//!           ).expect("HPO should be well formatted");
+//! let hpo = Arc::new(hpo);
+//! // Configure Fenominal
+//! let fenominal = Fenominal::new(hpo);
+//! ```
+//!
+//! ## Use Fenominal
+//! 
+//! The input to fenominal is a text that may contain 
+//! [Human Phenotype Ontology (HPO)](https://hpo.jax.org/app/) term labels.
+//! 
+//!
+//! ### Example
+//! 
+//! Get [`fenominal::FenominalHit`]s for an example text:
+//! 
+//! ```
+//! use std::fs::File;
+//! use std::io::BufReader;
+//! use std::sync::Arc;
+//! use flate2::bufread::GzDecoder;
+//! use fenominal::fenominal::Fenominal;
+//! use ontolius::io::OntologyLoaderBuilder;
+//! use ontolius::ontology::csr::FullCsrOntology;
+//! let hp_path = "resources/hp.v2025-03-03.json.gz";
+//! let loader = OntologyLoaderBuilder::new().obographs_parser().build();
+//! let hpo: FullCsrOntology = loader.load_from_read(
+//!              GzDecoder::new(BufReader::new(File::open(hp_path).expect("HPO should be readable")))
+//!            ).expect("HPO should be well formatted");
+//! let hpo = Arc::new(hpo);
+//! let fenominal = Fenominal::new(hpo);
+//! use fenominal::fenominal::FenominalHit;
+//!
+//! // Perform text mining
+//! let text = "Intellectual disability, macrocephaly, scoliosis";
+//! let hits: Vec<FenominalHit> = fenominal.process(&text);
+//!
+//! let labels: Vec<_> = hits.iter().map(|hit| &hit.label).collect();
+//! assert_eq!(labels, &["Intellectual disability", "Macrocephaly", "Scoliosis"]);
+//! ```
+//!
+//! 
+
 mod core_document;
 pub mod fenominal;
 mod hpo;
